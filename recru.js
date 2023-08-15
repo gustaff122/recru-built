c18f562e8660034185b43df9193dc45cc088991d
gustaff122 <xseoos@gmail.com>
2023-08-15 21:03:40 +0200

    fix

--- recru.js
+++ recru.js
@@ -8,6 +8,6 @@ app.get("*", (req, res) => {
   res.sendFile(__dirname + "/fake-products-management/index.html");
 });
 
-app.listen(2240, () => {
+app.listen(1240, () => {
   console.log("Recru - PORT 1240");
 });
